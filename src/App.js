7ea1ba4983485fa149bac57419bfa8cf510d54e5
hasan <hsnnkb.dev@gmail.com>
2023-04-24 22:15:05 +0100

    Add styling to homepage, Add navbar, BrowserRouter Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Home></Home>
-      <Footer></Footer>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 export default function Navigation() {
    
   return (
-    <nav className='Navigation'>
-      <section className='MainLinks'>
-        {/* <Link className={'Link'} to='/'>Home</Link> */}
-      <a href="#">Home</a>
+    <nav className="Navigation">
+      <section className="MainLinks">
+        <Link className={"Link"} to="/">
+          Home
+        </Link>
+        <Link className={"Link"} to="/Sessions">
+          Sessions
+        </Link>
       </section>
     </nav>
-  )
+  );
 }
