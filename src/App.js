f4d376043e5136c35ef888337557b8ad878afe24
hasan <hsnnkb.dev@gmail.com>
2023-04-27 15:05:15 +0100

    Add react-google-map package, Add map styling, Add sessions to map

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import ExamList from './components/ExamList';
+import ExamMap from './components/ExamMap';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sessions" element={<ExamList />} />
+          <Route path="/exam-map" element={<ExamMap />} />
         </Routes>
         <Footer></Footer>
       </div>
